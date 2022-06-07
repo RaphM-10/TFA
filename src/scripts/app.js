4f2aad9f181d30c8259b1d5d71a09546bb092e8b
Raphaël <raphael.mauclet@gmail.com>
2022-06-07 07:16:34 +0200

    Optimisation des images 2x et ajout de gsap

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,127 @@
-console.info('Hello world');
+import { gsap } from "gsap"; 
+console.log(gsap.version);
+
+import ScrollTrigger from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".valeur-mockup", {
+    y:-500,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".valeur-mockup",
+        start: "140% 75%",
+        end: "130% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+        //markers: true
+    }
+});
+
+gsap.from(".list-el__texte--1", {
+    x:500,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".list-el__texte--1",
+        start: "20% 75%",
+        end: "-20% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+    }
+});
+gsap.from(".list-el__texte--2", {
+    x:500,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".list-el__texte--2",
+        start: "20% 75%",
+        end: "-20% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+    }
+});gsap.from(".list-el__texte--3", {
+    x:500,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".list-el__texte--3",
+        start: "20% 75%",
+        end: "-20% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+    }
+});
+
+gsap.from(".photo--sac", {
+    scale:0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".photo--sac",
+        start: "60% 75%",
+        end: "60% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+    }
+});
+
+gsap.from(".photo--trousse", {
+    scale:0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".photo--trousse",
+        start: "60% 75%",
+        end: "60% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+    }
+});
+
+gsap.from(".photo--jupe", {
+    scale:0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".photo--jupe",
+        start: "60% 75%",
+        end: "60% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+    }
+});
+
+gsap.from(".photo--cor", {
+    scale:0.9,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".photo--cor",
+        start: "60% 75%",
+        end: "60% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+    }
+});
+
+gsap.from(".nav-avis__el", {
+    x:-500,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".nav-avis__el",
+        start: "20% 75%",
+        end: "-10% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+        
+    }
+});
+
+gsap.from(".mockup", {
+    y:-400,
+    opacity: 0,
+    scrollTrigger: {
+        trigger:".mockup",
+        start: "80% 75%",
+        end: "90% 25%",
+        toggleActions: "play pause reverse reset",
+        scrub: true,
+        
+    }
+});
+
+
